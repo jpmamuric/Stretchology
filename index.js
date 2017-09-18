160a4ec691d8feb478743c4af5efdacd259e608d
jp <webdev.jmamuric@gmail.com>
2017-09-18 15:42:48 -0700

    added stripe webhook

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose.connect(config.mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.raw({ type: '*/*' }));
 app.use(bodyParser.json());
 app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [config.cookieKey] }));
 app.use(passport.initialize());
--- routes/billing_routes.js
+++ routes/billing_routes.js
@@ -1,6 +1,7 @@
 const config  = require('../config/keys');
-const stripe  = require('stripe')(config.stripeSK);
+const stripe  = require('stripe')(config.stripe.secretKey);
 const requireLogin = require('../middlewares/require_login');
+const endpointSecret = config.stripe.webhookSecretKey;
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
@@ -19,5 +20,17 @@ module.exports = app => {
     const user = await req.user.save();
     res.send(user);
 
+  });
+
+  app.post('/api/stripe/webhooks', (req, res) => {
+    try {
+      let signature = req.headers["stripe-signature"];
+      let event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
+      console.log(event);
+      res.status(200).send({ recieved: true });
+    } catch (err) {
+      console.log('Error', err.message)
+      return res.status(422).send(`Webhook Error${err.message}`);
+    }
   });
 }
